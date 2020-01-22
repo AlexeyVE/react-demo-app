98456f0b7b36f9feefff8e273d402741dc4a6e1e
Alexeyve <sle8882@gmail.com>
2020-01-22 15:56:21 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Content from './components/Content';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <Header/>
+      <Nav/>
+      <Content/>        
+    </div>
+  );
+}
+
+export default App;
